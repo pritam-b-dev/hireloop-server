ac872c252a39bef4d05966a2b1f457a971961f94
pritam-b-dev <pritam.bhowmick@ymail.com>
2026-06-16 18:33:16 +0600

    connected to created database and collection

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("hireloop_db");
+    const jobCollection = database.collection("jobs");
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
