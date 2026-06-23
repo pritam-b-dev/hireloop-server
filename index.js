e54a6425dc8916269cfef2223c618c499b6d7e75
pritam-b-dev <pritam.bhowmick@ymail.com>
2026-06-24 00:55:17 +0600

    post and get data for job application.

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ async function run() {
     const jobCollection = database.collection("jobs");
     const companyCollection = database.collection("companies");
     const userCollection = database.collection("user");
-
+    const applicationsCollection = database.collection("applications");
     //api
 
     app.get("/api/users", async (req, res) => {
@@ -71,6 +71,30 @@ async function run() {
         createdAt: new Date(),
       };
       const result = await jobCollection.insertOne(newJob);
+      res.send(result);
+    });
+
+    //application related api
+    app.get("/api/applications", async (req, res) => {
+      const query = {};
+      if (req.query.applicantId) {
+        query.applicantId = req.query.applicantId;
+      }
+      if (req.query.jobId) {
+        query.jobId = req.query.jobId;
+      }
+      const cursor = applicationsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/api/applications", async (req, res) => {
+      const application = req.body;
+      const newApplication = {
+        ...application,
+        createdAt: new Date(),
+      };
+      const result = await applicationsCollection.insertOne(newApplication);
       res.send(result);
     });
 
